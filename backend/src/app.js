51ef517ee15fc58b513cb8a67b9c0e29cb1b8d9a
Akilrizwani <akilrizwani627@gmail.com>
2026-04-08 21:28:00 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,7 @@ app.use(cookieparser());
 app.use(express.static("public"));
 app.use(urlencoded({ extended: true }));
 app.use(cors({
-    origin: "https://connecthub-frontend-006.onrender.com",
+    origin: "https://connecthub-frontend-0016.onrender.com",
     credentials: true,
 }));
 app.use(express.urlencoded({ extended: true }));
