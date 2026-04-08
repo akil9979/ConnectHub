0c36d91fc23abd6e9479e1581068781c0410355c
Akilrizwani <akilrizwani627@gmail.com>
2026-04-08 21:09:13 +0530

    Update CORS origin to production URL

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,7 @@ app.use(cookieparser());
 app.use(express.static("public"));
 app.use(urlencoded({ extended: true }));
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://connecthub-frontend-00l6.onrender.com",
     credentials: true,
 }));
 app.use(express.urlencoded({ extended: true }));
